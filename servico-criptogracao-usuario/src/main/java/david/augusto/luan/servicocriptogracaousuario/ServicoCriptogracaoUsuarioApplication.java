4e75f27cc97359d2bdd83f422079686d032f30ca
luan-alencar <luanalencar134@gmail.com>
2021-10-01 09:41:58 -0300

    primeiro commit

---
+++ servico-criptogracao-usuario/src/main/java/david/augusto/luan/servicocriptogracaousuario/ServicoCriptogracaoUsuarioApplication.java
@@ -0,0 +1,13 @@
+package david.augusto.luan.servicocriptogracaousuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoCriptogracaoUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoCriptogracaoUsuarioApplication.class, args);
+	}
+
+}
