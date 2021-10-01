6dcf5aba4e8e08d720c0cb39a2a2d10adc05f248
luan-alencar <luanalencar134@gmail.com>
2021-10-01 10:39:45 -0300

    backend rodando

--- servico-criptogracao-usuario/src/main/java/david/augusto/luan/servicocriptogracaousuario/ServicoCriptogracaoUsuarioApplication.java
+++ servico-criptogracao-usuario/src/main/java/david/augusto/luan/servicocriptogracaousuario/ServicoCriptogracaoUsuarioApplication.java
@@ -2,12 +2,21 @@ package david.augusto.luan.servicocriptogracaousuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ServicoCriptogracaoUsuarioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServicoCriptogracaoUsuarioApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServicoCriptogracaoUsuarioApplication.class, args);
+    }
 
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
